2111a7731bf229a1c146eb19c4c21e27104af914
arslanjajja1 <arslanjajja13@gmail.com>
2023-11-28 13:01:03 +0500

    # Remove Duplicates From Linked List

---
+++ Problems/0.remove_duplicates_from_linkedlist.py
@@ -0,0 +1,34 @@
+# Remove Duplicates From Linked List 
+###
+# You're given the head of a Singly Linked List whose nodes are in sorted order with respect to their values. Write a function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values. The Linked List should be modified in place (i.e., you shouldn't create a brand new list), and the modified Linked List should still have its nodes sorted with respect to their values.
+
+# Each LinkedList node has an integer value as well as a next node pointing to the next node in the list or to None / null if it's the tail of the list.
+
+# Sample Input
+# linkedList = 1 -> 1 -> 3 -> 4 -> 4 -> 4 -> 5 -> 6 -> 6 // the head node with value 1
+# Sample Output
+# 1 -> 3 -> 4 -> 5 -> 6 // the head node with value 1
+
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    # Write your code here.
+    dummy = LinkedList(0)
+    dummy.next = linkedList
+    prev = dummy
+    curr = dummy.next
+    while curr:
+        if curr.value != prev.value:
+            prev.next = curr
+            prev = curr
+        curr = curr.next
+    prev.next = None
+    return linkedList
+
+    # TC : O(N)
+    # SC : O(1)
